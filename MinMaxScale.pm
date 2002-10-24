f5f778cf9de344e628c2299b2a6c01683f7a603b
Jean-Pierre Vidal <jpvidal@cpan.org>
2002-10-24 20:46:58 +0000

    Import of JPVIDAL/Tk-MinMaxScale-0.02 from CPAN.
    gitpan-cpan-distribution: Tk-MinMaxScale
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JPVIDAL/Tk-MinMaxScale-0.02.tar.gz
    gitpan-cpan-author:       JPVIDAL
    gitpan-cpan-maturity:     released

--- MinMaxScale.pm
+++ MinMaxScale.pm
@@ -1,17 +1,25 @@
 package Tk::MinMaxScale;
 use warnings;
-use Tk;
 use Carp;
-use base qw(Tk::Toplevel);
-use Tk::widgets qw(Frame Scale);
+
 @ISA = qw(Tk::Frame);
 
-our $VERSION = '0.01';
+$VERSION = '0.02';
 
 Construct Tk::Widget 'MinMaxScale';
 
+#sub ClassInit {
+#	my ($class,$mw) = @_;
+#	$class->SUPER::ClassInit($mw);
+#}
+
 sub Populate {
-	my ($minvar, $oldmin, $maxvar, $oldmax, $cmd);
+	my ($smin, $smax); # the scales
+	my ($minvar, $maxvar); # references of variables associated with the scales
+	my ($oldmin, $oldmax); # previous values of the variables
+	my $cmd; # reference to a callback associated with any change
+	my $shifted; # is Shift key pressed ?
+
 	my ($cw, $args) = @_;
 	$cw->SUPER::Populate($args);
 
@@ -28,12 +36,23 @@ sub Populate {
 	$cmd = delete $args->{'-command'};
 
 	# create the subwidget 'min' Scale
-	my $smin = $cw->Scale(
+	$smin = $cw->Scale(
 		%$args,
 		-variable => $minvar,
 		-label => $minlbl,
 		-command => sub {
-			$$maxvar = $$minvar if $$minvar > $$maxvar;
+			if ($shifted) {
+				my $distance = $oldmax - $oldmin; # distance between sliders
+				my $distancemin = $args->{'-to'} - $$minvar; # distance between min slider and maximum
+				if ($distancemin < $distance) {
+					$$minvar = $$maxvar - $distance;
+					return;
+				} else {
+					$$maxvar = $$minvar + $distance;
+				}
+			} else {
+				$$maxvar = $$minvar if $$minvar > $$maxvar;
+			}
 			if (($$minvar != $oldmin) || ($$maxvar != $oldmax)) {
 				$oldmin = $$minvar;
 				$oldmax = $$maxvar;
@@ -43,12 +62,23 @@ sub Populate {
 	)->pack;
 
 	# create the subwidget 'max' Scale
-	my $smax = $cw->Scale(
+	$smax = $cw->Scale(
 		%$args,
 		-variable => $maxvar,
 		-label => $maxlbl,
 		-command => sub {
-			$$minvar = $$maxvar if $$maxvar < $$minvar;
+			if ($shifted) {
+				my $distance = $oldmax - $oldmin; # distance between sliders
+				my $distancemax = $$maxvar - $args->{'-from'}; # distance between minimum and max slider
+				if ($distancemax < $distance) {
+					$$maxvar = $$minvar + $distance;
+					return;
+				} else {
+					$$minvar = $$maxvar - $distance;
+				}
+			} else {
+				$$minvar = $$maxvar if $$maxvar < $$minvar;
+			}
 			if (($$minvar != $oldmin) || ($$maxvar != $oldmax)) {
 				$oldmin = $$minvar;
 				$oldmax = $$maxvar;
@@ -60,6 +90,15 @@ sub Populate {
 	$cw->ConfigSpecs (
 		DEFAULT => [PASSIVE],
 	);
+
+	$cw->bind($smin, "<Shift-Button-1>", sub { $shifted = 1; } );
+	$cw->bind($smax, "<Shift-Button-1>", sub { $shifted = 1; } );
+	$cw->bind($smin, "<Shift-ButtonRelease-1>", sub { $shifted = 0; });
+	$cw->bind($smax, "<Shift-ButtonRelease-1>", sub { $shifted = 0; });
+}
+
+sub get {
+	return ($_[0]);
 }
 
 1;
@@ -72,20 +111,34 @@ Tk::MinMaxScale - Two Scale(s) to get a (min, max) values pair
 
 =head1 SYNOPSIS
 
-	I<$range> = I<$parent>-E<gt>B<MinMaxScale>(I<-option> =E<gt> I<value>, ... );
+I<$mms> = I<$parent>-E<gt>B<MinMaxScale>(I<-option> =E<gt> I<value>, ... );
+
+I<$mms> = I<$parent>-E<gt>B<MinMaxScale>(
+    -variablemin =E<gt> \$vn,
+    -variablemax =E<gt> \$vx,
+    -labelmin =E<gt> '...',
+    -labelmax =E<gt> '...',
+    ...,
+);
 
 =head1 DESCRIPTION
 
 Tk::MinMaxScale is a Frame-based widget including two Scale widgets, the first
-acting as a "minimum" and the second as a "maximum", the value of the first always
-less than or equal to the value of the second.
-The purpose of MinMaxScale is to retrieve a range of values narrower than the whole Scale range
-given by the Scale options '-from' and '-to' applied to both "minimum" and "maximum" Scale.
-This is done through the "Scale variable" -variablemin and -variablemax.
+acting as a "minimum" and the second as a "maximum". The value of "minimum" is always
+less than or equal to the value of "maximum".
 
-The widget accept all options accepted by Scale, except -variable.
+The purpose of Tk::MinMaxScale is to get a range of values narrower than the whole Scale range
+given by the options B<-from> and B<-to> (applied to both "minimum" and "maximum" Scale).
+This is done through the options B<-variablemin> and B<-variablemax>, see below.
 
+In addition, dragging a slider while pressing a B<Shift> key drags both sliders, locking their distance. You must hold down the shift key before dragging the slider.
+
+=head1 OPTIONS
+
+The widget accept all options accepted by B<Scale>, except B<-variable>.
 In addition, the following option/value pairs are supported:
+
+=over 4
 
 =item B<-labelmin>
 
@@ -93,27 +146,48 @@ The text used as a label for the "minimum" Scale. Default none.
 
 =item B<-labelmax>
 
-The text used as a label for the "maximum" Scale Default none..
+The text used as a label for the "maximum" Scale. Default none.
 
 =item B<-variablemin>
 
-The name of a global variable to link to the "minimum" Scale.
+A reference to a global variable linked with the "minimum" Scale. Required.
 
 =item B<-variablemax>
 
-The name of a global variable to link to the "maximum" Scale.
+A reference to a global variable linked with the "maximum" Scale. Required.
 
-=head2 EXPORT
+=back
+
+=head1 EXPORT
 
 None.
 
 =head1 METHODS
 
-At this time, there is no method provided for this widget.
+None.
 
 =head1 HISTORY
 
-version 0.01 2002/10/17 first release.
+=item B<version 0.02> - 2002/10/24
+
+=over 2
+
+=item -
+
+new feature added: dragging a slider while pressing a shift key
+drags both sliders, locking their distance (an idea from Mark Lakata).
+
+=back
+
+=item B<version 0.01> - 2002/10/17
+
+=over 2
+
+=item -
+
+first release.
+
+=back
 
 =head1 TODO
 
@@ -129,8 +203,10 @@ This package is free software and is provided "as is"
 without express or implied warranty. It may be used, modified,
 and redistributed under the same terms as Perl itself.
 
+Feedback would be appreciated in many ways, including corrections to my poor english.
+
 =head1 SEE ALSO
 
-L<Tk::Scale>.
+B<Tk::Scale>.
 
 =cut
---
+++ example.pl
@@ -0,0 +1,44 @@
+#!perl -w
+use strict;
+use Tk;
+use Tk::MinMaxScale;
+
+my $vn = 94;
+my $vx = 117;
+my $wn = 65;
+my $wx = 86;
+
+my $top = new MainWindow;
+
+my $mms1 = $top->MinMaxScale(
+	-from => 50.0,
+	-to => 150.0,
+	-orient => 'horizontal',
+	-resolution => 0.1,
+	-command => \&s1,
+	-labelmin => 'mini',
+	-labelmax => 'max',
+	-label => 'minmax',
+	-variablemin => \$vn,
+	-variablemax => \$vx,
+)->pack;
+
+my $mms2 = $top->MinMaxScale(
+	-from => 30,
+	-to => 120,
+	-orient => 'horizontal',
+	-resolution => 1,
+	-command => \&s2,
+	-label => 'min-max',
+	-variablemin => \$wn,
+	-variablemax => \$wx,
+)->pack;
+
+MainLoop;
+
+sub s1 {
+	print "mms 1 $vn..$vx\n";
+}
+sub s2{
+	print "mms 2 $wn..$wx\n";
+}
